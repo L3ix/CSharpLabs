b457e711972f02e78e076029e5b5e267dfb3e633
Maxim <Maxim>
2023-06-18 01:31:46 +0300

    first commit

---
+++ Classes/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using HelloApp.Classes.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloApp.Classes
+{
+    class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+
+
+
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using HelloApp.Classes;
+using HelloApp.Classes.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace HelloApp
+{
+    class Program
+    { 
+        public static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Database.ExecuteSqlRaw("TRUNCATE TABLE [Users];");
+
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                var users = db.Users.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (User u in users)
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+            }
+            Console.Read();
+        }
+    }
+}
